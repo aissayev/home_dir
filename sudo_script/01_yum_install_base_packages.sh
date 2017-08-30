ca3e514e0bc6194d1fbadff8b3fa535f9c5b35ba
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-08-30 08:32:31 -0400

    Work in progress

--- sudo_script/01_yum_install_base_packages.sh
+++ sudo_script/01_yum_install_base_packages.sh
@@ -10,8 +10,6 @@ if [ -n "$EXTENDED_BOOTSTRAP" ]; then
       automake \
       bash \
       bash-completion \
-      bash-doc \
-      bzip2 \
       colordiff \
       ctags-etags \
       curl \
@@ -21,19 +19,16 @@ if [ -n "$EXTENDED_BOOTSTRAP" ]; then
       git \
       htop \
       kernel-devel-$(uname -r) \
-      libntlm \
-      libntlm-devel \
-      libyaml \
-      libyaml-devel \
-      ncurses-devel \
       net-tools \
+      readline \
       screen \
+      the_silver_searcher \
       tmux \
-      unzip \
       uuid \
       uuid-devel \
       vim-enhanced \
       wget \
-      zsh;
+      zsh \
+      zsh-completion;
   fi
 fi
